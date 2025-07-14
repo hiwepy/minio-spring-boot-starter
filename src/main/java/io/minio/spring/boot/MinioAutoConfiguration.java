d6f140524b313526a96641702b501fb5a1a0ee93
luffy <luffy@uzx.com>
2025-07-14 15:08:59 +0800

    build: 更新 MinIO 版本并调整项目配置
    - 将 MinIO 版本从 8.5.13 升级到 8.5.17
    - 更新项目版本号为3.3.x.20250712-SNAPSHOT
    - 在 Spring Boot 自动配置中添加 MinioAutoConfiguration 导入

--- src/main/java/io/minio/spring/boot/MinioAutoConfiguration.java
+++ src/main/java/io/minio/spring/boot/MinioAutoConfiguration.java
@@ -6,13 +6,13 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.util.StringUtils;
 
 import io.minio.MinioClient;
 import io.minio.errors.MinioException;
 
 @Configuration
-@ConditionalOnClass(MinioClient.class)
 @EnableConfigurationProperties({ MinioProperties.class })
 public class MinioAutoConfiguration{
 
